444867c66a5d80e8221e27e8e3928a538ca3e718
heemanglee <wishhopelee@gmail.com>
2025-09-13 11:33:57 +0900

    feat: Pydantic 검증 실패 시 400 에러를 반환하도록 핸들러 추가

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from fastapi.requests import Request
+from fastapi.responses import JSONResponse
 
 from user.interface.controller.user_controller import user_router
 
 app = FastAPI()
 app.include_router(user_router)
+
+@app.exception_handler(RequestValidationError)
+async def request_validation_exception_handler(
+        request: Request,
+        exception: RequestValidationError
+):
+    return JSONResponse(
+        status_code=400,
+        content=exception.errors()
+    )
