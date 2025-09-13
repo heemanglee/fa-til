cd2c1489f89d25764a57e4b36321f19a44e00e20
heemanglee <wishhopelee@gmail.com>
2025-09-13 12:40:29 +0900

    feat: find_by_email 로직 추가

--- user/infra/repository/user_repo.py
+++ user/infra/repository/user_repo.py
@@ -1,3 +1,5 @@
+from fastapi import HTTPException
+
 from database import SessionLocal
 from user.domain.user import User as UserVO
 from user.infra.model.user import User
@@ -24,4 +26,17 @@ class UserRepository(IUserRepository):
                 db.close()
 
     def find_by_email(self, email: str) -> User :
-        pass
+        with SessionLocal() as db:
+            find_user = db.query(User).filter(User.email == email).first()
+
+        if find_user:
+            return UserVO(
+                id=find_user.id,
+                name=find_user.name,
+                email=find_user.email,
+                password=find_user.password,
+                created_at=find_user.created_at,
+                updated_at=find_user.updated_at
+            )
+
+        raise HTTPException(status_code=422)
